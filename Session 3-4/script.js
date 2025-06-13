63fcaf8233a559bdf05b2c0b013effd659fe7696
Kunal Madoliya <madoliyakunal2005@gmail.com>
2025-06-13 14:59:24 +0530

    Saved my local changes before pull

--- Session 3-4/script.js
+++ Session 3-4/script.js
@@ -44,6 +44,13 @@
 //   console.log("c is bigger");
 // }
 
-
 // ---------------------Accept a year and check if it a leap year or not (google to find out what's a
 // leap year)-----------------------------------
+
+let year = Number(prompt("Enter year:-"));
+
+if ((year % 4 === 0 && year % 100 === 0) || year % 400 === 0) {
+  console.log("leap year");
+} else {
+  console.log("not a leap year");
+}
