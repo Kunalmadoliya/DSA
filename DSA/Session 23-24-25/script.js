67ca83a72bd6ea33ae3ad6ff62eabc7532e603f4
Kunal Madoliya <madoliyakunal2005@gmail.com>
2025-07-14 12:14:14 +0530

    14-07-25

--- DSA/Session 23-24-25/script.js
+++ DSA/Session 23-24-25/script.js
@@ -63,18 +63,82 @@
 
 // --Q 87. 53. Maximum Subarray / Kadane's Algo
 
-let arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+// let arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
 
-let sum = 0;
-let maxSum = 0;
+// let sum = 0;
+// let maxSum = 0;
 
-for (let i = 0; i < arr.length; i++) {
-  sum = sum + arr[i];
-  if (sum > maxSum) {
-    maxSum = sum;
+// for (let i = 0; i < arr.length; i++) {
+//   sum = sum + arr[i];
+//   if (sum > maxSum) {
+//     maxSum = sum;
+// }
+// if (sum < 0) {
+//   sum = 0;
+// }
+// }
+// console.log(maxSum);
+
+// let arr = [3, 2, 3];
+// let map = new Map();
+// let max = 0
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (map.has(arr[i])) {
+//     map.set(arr[i], map.get(arr[i]) + 1);
+//   } else {
+//     map.set(arr[i], 1);
+//   }
+// }
+
+// for (const [key,value] of map) {
+//    if(value > max){
+//       max = key
+//    }
+
+// }
+
+// console.log(max);
+
+// console.log(map);
+
+// 121. Best Time to Buy and Sell Stock
+
+// let arr = [7, 1, 5, 3, 6, 4];
+// let small = arr[0];
+// let max = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (small > arr[i]) {
+//     small = arr[i];
+//   }
+//   for (let i = arr[small]; i < arr.length; i++) {
+//     if (max < arr[i]) {
+//       max = arr[i];
+//     }
+//   }
+// }
+
+// let profit = max - small;
+
+// console.log(profit);
+
+let nums = [2, 0, 2, 1, 1, 0];
+let i = 0,
+  j = 0,
+  k = nums.length - 1;
+
+while ( i <= k) {
+  if (nums[i] === 0) {
+    [nums[i], nums[j]] = [nums[j], nums[i]];
+    i++;
+    j++;
+  } else if (nums[i] === 2) {
+    [nums[i], nums[k]] = [nums[k] , nums[i]];
+    k--;
+  } else {
+    i++;
+  }
 }
-if (sum < 0) {
-  sum = 0;
-}
-}
-console.log(maxSum);
+
+console.log(nums);
