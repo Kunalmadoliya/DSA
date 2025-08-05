5ff21ab205022c7115fa88d92d8c45a392cdf0ac
Kunal Madoliya <madoliyakunal2005@gmail.com>
2025-08-05 15:39:49 +0530

    5-08-25

---
+++ DSA PP/Sorting/script.js
@@ -0,0 +1,44 @@
+// selection sort
+
+let arr = [64, 25, 12, 22, 11];
+
+// let i = 0;
+
+// while (i < arr.length) {
+//   let maxIndex = i;
+//   let j = i + 1;
+
+//   while( j < arr.length ){
+//     if(arr[j] < arr[maxIndex]){
+//         maxIndex = j
+//     }
+//     j++
+//   }
+
+//   let temp = arr[i]
+//   arr[i] = arr[maxIndex]
+//   arr[maxIndex] = temp
+
+//   i++
+// }
+
+// console.log(arr);
+
+// bubble sort
+
+// for (let i = arr.length - 1; i >= 1; i--) {
+//   let didSwap = 0;
+//   for (let j = 0; j <= i - 1; j++) {
+//     if (arr[j] > arr[j + 1]) {
+//       let temp = arr[j];
+//       arr[j] = arr[j + 1];
+//       arr[j + 1] = temp;
+//       didSwap = 1;
+//     }
+//   }
+
+//   if (didSwap === 0) {
+//     break;
+//   }
+// }
+// console.log(arr);
