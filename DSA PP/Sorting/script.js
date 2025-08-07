f9b494ae51b464c33375ecbbfe0392439dc59cf3
Kunal Madoliya <madoliyakunal2005@gmail.com>
2025-08-07 17:02:33 +0530

    07-06-25

--- DSA PP/Sorting/script.js
+++ DSA PP/Sorting/script.js
@@ -55,41 +55,88 @@
 // }
 // console.log(arr);
 
-function mergeSort(arr, low, high) {
-if (low >= high) return;
+// function mergeSort(arr, low, high) {
+// if (low >= high) return;
 
-let mid = Math.floor((low + high) / 2);
-mergeSort(arr, low, mid);       
-mergeSort(arr, mid + 1, high);  
-merge(arr, low, mid, high);    
-}
+// let mid = Math.floor((low + high) / 2);
+// mergeSort(arr, low, mid);
+// mergeSort(arr, mid + 1, high);
+// merge(arr, low, mid, high);
+// }
 
-function merge(arr, low, mid, high) {
-let left = low,
-   right = mid + 1,
-   temp = [];
+// function merge(arr, low, mid, high) {
+// let left = low,
+//    right = mid + 1,
+//    temp = [];
 
-while (left <= mid && right <= high) {
- if (arr[left] < arr[right]) {
-   temp.push(arr[left++]);
- } else {
-   temp.push(arr[right++]);
- }
-}
+// while (left <= mid && right <= high) {
+//  if (arr[left] < arr[right]) {
+//    temp.push(arr[left++]);
+//  } else {
+//    temp.push(arr[right++]);
+//  }
+// }
 
-while (left <= mid) {
- temp.push(arr[left++]);
-}
+// while (left <= mid) {
+//  temp.push(arr[left++]);
+// }
 
-while (right <= high) {
- temp.push(arr[right++]);
-}
+// while (right <= high) {
+//  temp.push(arr[right++]);
+// }
 
-for (let i = low; i <= high; i++) {
- arr[i] = temp[i - low];
-}
-}
+// for (let i = low; i <= high; i++) {
+//  arr[i] = temp[i - low];
+// }
+// }
+
+// mergeSort(arr, 0, arr.length - 1);
+// console.log(arr)
+
+
+
+
+// quickSort(arr, 0, arr.length - 1);
+// console.log(arr); 
+
+// function quickSort(arr, first, last) {
+//   if (first >= last) return;
+
+//   let pIndex = findIndex(arr, first, last);
+//   quickSort(arr, first, pIndex - 1);
+//   quickSort(arr, pIndex + 1, last);
+// }
+
+// function findIndex(arr, first, last) {
+//   let pivot = arr[last];
+//   let i = first - 1;
+
+//   for (let j = first; j < last; j++) {
+//     if (arr[j] < pivot) {
+//       i++;
+//       swap(arr, i, j);
+//     }
+//   }
+
+//   swap(arr, i + 1, last); 
+//   return i + 1;           // Return pivot index
+// }
+
+// function swap(arr, i, j) {
+//   let temp = arr[i];
+//   arr[i] = arr[j];
+//   arr[j] = temp;
+// }
 
 let arr = [38, 27, 43, 3, 9, 82, 10];
-mergeSort(arr, 0, arr.length - 1);
-console.log(arr)
+
+let i = 0 , j = arr.length - 1
+
+while( i <= j){
+  let temp = arr[i]
+  arr[i]  = arr[j]
+  arr[j] = temp
+  i++ 
+  j--
+}
+console.log(arr);
